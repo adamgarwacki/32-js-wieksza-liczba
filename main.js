ba58715c7a0f752f97efe6b329e88cade348b77d
Adam Garwacki <adam.garwacki@gmail.com>
2020-03-30 09:47:21 +0200

    Stworzenie pliku 'main.js'. Stworzenie funkcji zwracającej większą z dwóch liczb.

---
+++ main.js
@@ -0,0 +1,12 @@
+let num1 = 5;
+let num2 = 2;
+
+function largerNumber(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+console.log(largerNumber(num1, num2));
